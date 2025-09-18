6cfe3e65ce442538f058d9e02bc42335fb0205cb
mayank20011 <mayank.behl.mb1@gmail.com>
2025-09-18 13:11:47 +0530

    change specific swiper from preview ones to normal

--- src/components/common/SwiperWithImagePreviewsAtBottom.jsx
+++ src/components/common/SwiperWithImagePreviewsAtBottom.jsx
@@ -1,42 +1,16 @@
-import { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-import { FreeMode, Thumbs } from "swiper/modules";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
 
 export default function SwiperWithImagePreviewsAtBottom({ data }) {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-
   return (
     <>
-      <Swiper
-        loop={true}
-        spaceBetween={10}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Thumbs]}
-        className="mySwiper2"
-      >
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         {data.map((imgSrc, id) => (
           <SwiperSlide key={id}>
-            <img src={imgSrc} alt="Vardaan Farms Products Image"/>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        loop={true}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Thumbs]}
-        className="mySwiper"
-      >
-        {data.map((imgSrc, id) => (
-          <SwiperSlide key={id}>
-            <img src={imgSrc} alt="vardaan Farms Products Images"/>
+            <img src={imgSrc} alt="Vardaan Farms Products Image" />
           </SwiperSlide>
         ))}
       </Swiper>
